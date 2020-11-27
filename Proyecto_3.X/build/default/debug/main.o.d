14f494a58532886a6c28dcb9819d995d309a4567
mun19508 <68123854+mun19508@users.noreply.github.com>
2020-11-27 02:50:36 -0600

    nuevo comienzo

--- Proyecto_3.X/build/default/debug/main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/main.o:  \
-	C:/Users/Daniel/Documents/GitHub/Proyecto3_ControlComputarizado/Proyecto_3.X/main.asm  \
-
--- Proyecto_3.X/build/default/production/main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/main.o:  \
-	C:/Users/Daniel/Documents/GitHub/Proyecto3_ControlComputarizado/Proyecto_3.X/main.asm  \
-
