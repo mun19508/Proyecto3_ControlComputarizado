6b1c908caa70b234dfa73775dc73c93bf1dca533
mun19508 <68123854+mun19508@users.noreply.github.com>
2020-11-27 00:46:08 -0600

    F motores

---
+++ Proyecto_3.X/build/default/debug/main.o.d
@@ -0,0 +1,3 @@
+build/default/debug/main.o:  \
+	C:/Users/Daniel/Documents/GitHub/Proyecto3_ControlComputarizado/Proyecto_3.X/main.asm  \
+
--- Proyecto_3.X/main.asm
+++ Proyecto_3.X/main.asm
@@ -1,10 +1,10 @@
 #include "p16f887.inc"
 
 ; CONFIG1
-; __config 0xE0D4
+; __config 0x20D4
  __CONFIG _CONFIG1, _FOSC_INTRC_NOCLKOUT & _WDTE_OFF & _PWRTE_OFF & _MCLRE_OFF & _CP_OFF & _CPD_OFF & _BOREN_OFF & _IESO_OFF & _FCMEN_OFF & _LVP_OFF
 ; CONFIG2
-; __config 0xFFFF
+; __config 0x3FFF
  __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
 
 PR_VAR		    UDATA
@@ -35,39 +35,9 @@ SAVE:					;Sirve para guardar el valor actual de:
     SWAPF	STATUS, W
     MOVWF	TEMP_STATUS		;& STATUS
 ISR:
-    BTFSS	PIR1, ADIF
-    GOTO		ADC_INT
     BTFSS	INTCON,T0IF
-    GOTO		TMR0_INT
-    GOTO		LOAD
-ADC_INT:    
-    BCF		PIR1, ADIF
-    MOVF		CONT_ADC, W
-    ADDWF	PCL, F
-    GOTO		SERVO0_ADC
-    GOTO		SERVO1_ADC
-    GOTO		SERVO2_ADC
-    CLRF		CONT_ADC
-SERVO0_ADC:       
-    MOVFW	ADRESH
-    MOVWF	SERVO0
-    INCF		CONT_ADC, F
-    BSF		ADCON0, 2
-    GOTO		INICIAR_CONV			
-SERVO1_ADC:       
-    MOVFW	ADRESH
-    MOVWF	SERVO1
-    INCF		CONT_ADC, F
-    BSF		ADCON0, 3
-    BCF		ADCON0, 2
-    GOTO		INICIAR_CONV			
-SERVO2_ADC:       
-    MOVFW	ADRESH
-    MOVWF	SERVO2
-    INCF		CONT_ADC, F
-    BCF		ADCON0, 3
-    GOTO		INICIAR_CONV
-TMR0_INT:
+     GOTO	ADC_INT
+ TMR0_INT:
     BCF		INTCON,T0IF
     MOVF		CICLO,W
     ADDWF	PCL,F
@@ -75,69 +45,99 @@ TMR0_INT:
     GOTO		NEGATIVO
     CLRF		CICLO
 POSITIVO:
-    BSF		PORTC, RC3
+    BSF		PORTC, RC0
     CLRF		TMR0
     MOVF		CT1,W
     SUBWF	TMR0,F
     INCF		CICLO,F
-    GOTO		LOAD
+    GOTO		ADC_INT
 NEGATIVO:
-    BCF		PORTC, RC3
+    BCF		PORTC, RC0
     CLRF		TMR0
     INCF		CICLO, F
+   
+ADC_INT: 
+    BTFSS	PIR1, ADIF
     GOTO		LOAD
+    BCF		PIR1, ADIF
+    MOVF		CONT_ADC, W
+    ADDWF	PCL, F
+    
+    GOTO		SERVO0_ADC
+    GOTO		SERVO1_ADC
+    GOTO		SERVO2_ADC
+    CLRF		CONT_ADC
+    
+SERVO0_ADC:       
+    MOVFW	ADRESH
+    MOVWF	SERVO0
+    BCF		ADCON0, 3
+    BSF		ADCON0, 2		;Canal 1
+    GOTO		INICIAR_CONV		
+    
+SERVO1_ADC:       
+    MOVFW	ADRESH
+    MOVWF	SERVO1
+    BSF		ADCON0, 3
+    BCF		ADCON0, 2		 ;Canal 2
+    GOTO		INICIAR_CONV	
+    
+SERVO2_ADC:       
+    MOVFW	ADRESH
+    MOVWF	SERVO2
+    BCF		ADCON0, 3
+    BCF		ADCON0, 2		;Canal 0
+    GOTO		INICIAR_CONV
+    
 INICIAR_CONV:
-    NOP
-    NOP
-    NOP
-    NOP
-    NOP
+    INCF		CONT_ADC, F
     BSF		ADCON0, GO
+    
 LOAD:					;Se recupera el valor de:
     SWAPF	TEMP_STATUS, W
     MOVWF	STATUS		;STATUS
     SWAPF	TEMP_W, F
     SWAPF	TEMP_W, W		;& de "W"
-    BSF		INTCON, GIE		;se habilitan la interrupciones globales
     RETFIE
 ;----------------------------------------------------------------------------------------- 
 MAIN_PROG CODE			; let linker place main program
 START
     BSF		 STATUS, 6
     BSF		STATUS, 5		;--------------------Banco 3----------------
-    CLRF		ANSEL
-    BSF		ANSEL, 0		;Se habilita el analogico en A0
-    BSF		ANSEL, 1		;Se habilita el analogico en A1
-    BSF		ANSEL, 2		;Se habilita el analogico en A2
+    MOVLW	B'00000111'
+    MOVWF	ANSEL			;Se habilita el analogico en A0, A1 & A2
+    BCF		OPTION_REG, T0CS	;TMR0: Oscilador interno
+    BCF		OPTION_REG, PSA	;Prescaler a TMR0
+    BCF		OPTION_REG, PS2	
+    BSF		OPTION_REG, PS1	
+    BSF		OPTION_REG, PS0	;Prescaler de 1:16    
     BCF		STATUS, 6		;-------------------Banco 1------------------
     MOVLW	.255
     MOVWF	TRISA			;Puerto A como entrada.
     CLRF		TRISC			;Salida: Señales PMW. 
 ;----------------------------Configuración Timers----------------------------------------
-    MOVLW	.255			;TMR2:
-    MOVWF	PR2			;Aprox 4.09 ms.
-    BCF		OPTION_REG, T0CS	;TMR0: Oscilador interno
-    BCF		OPTION_REG, PSA	;Prescaler a TMR0
-    BCF		OPTION_REG, PS2	
-    BSF		OPTION_REG, PS1	
-    BSF		OPTION_REG, PS0	;Prescaler de 1:16
-;----------------------------Configuración Interrupciones--------------------------------
-    BSF		INTCON, GIE		;Se habilitaron la interrupciones globales,
-    BSF		INTCON, PEIE		;perifericas,
-    BSF		INTCON, T0IE 		;del TRM0.
-;-------------------------------------ADC----------------------------------------------- 
-   BSF		PIE1, ADIE		;y la interrupciones por el A/D.
-    BCF		ADCON1, ADFM		;Justificado a la izquierda.
-    BCF		ADCON1, VCFG1		;Ref: VSS.
-    BCF		ADCON1, VCFG0		;Ref: VDD.
-    BCF		STATUS, 5		;-------------------Banco 0------------------
-   MOVLW	B'01000001'		;Fosc/8, ANS0 & conversion activada.
-   MOVWF	ADCON0
-    BCF		PIR1, ADIF		;Se apaga la bandera del A/D.
-;-----------------------------------Timers pt2-------------------------------------------
-    BCF		PIR1, TMR2IF		;Se apaga la bandera de TMR2.
-    BSF		T2CON, TMR2ON	;Timer 2 encendido
-    BSF		T2CON, 1		;prescaler de 16	
+  BSF		INTCON,GIE		;Habilita las interrupciones globales
+  BSF		INTCON, PEIE		;& perifericas.
+      
+    BSF		PIE1, ADIE		;Interrupcion del ADC activa
+    ;---------------------------------------------------------------------------------------
+    MOVLW	.255 
+    MOVWF	PR2			;Periodo de 4.09 mS aproximadamente en PWM
+    CLRF		ADCON1   
+    BCF		STATUS, 5		;----------------BANCO 0-------------------   
+    BCF		INTCON, T0IF		;Se apaga la bandera
+    CLRF		TMR0	    
+    BSF		INTCON, T0IE		;Se habilita la interrrupcion del Timer 0.
+    
+    MOVLW	B'00001100' 
+    MOVWF	CCP1CON		;Modo de PWM en RC2
+    MOVWF	CCP2CON		; & RC1.
+    
+    MOVLW	B'0000110'		;TMR2 encendido con prescales de 1:16 
+    MOVWF	T2CON			;& postcaler de 1 
+    
+    MOVLW	B'01000001'		 ;ADC con FOSC/8, canal 0 
+    MOVWF	ADCON0		 ;& encendido.
 ;-------------------------------Limpiar variables/puertos--------------------------------
    CLRF		PORTA			;Se limpian los puertos.
    CLRF		PORTC
@@ -151,24 +151,32 @@ START
    CLRF		ANTIREB
    CLRF		CONT1
    BSF		ADCON0, GO
+   
 LOOP: 
-    MOVF		SERVO0, W
+    MOVF    	SERVO0,W
+    CALL		AJUSTE_CT
+    MOVF		ROTF, W   
     MOVWF	CCPR1L
+    
     MOVF		SERVO1, W
+    CALL		AJUSTE_CT
+    MOVF		ROTF, W    
     MOVWF	CCPR2L
+    
     MOVF		SERVO2, W
     CALL		AJUSTE_CT
+    MOVF		ROTF, W
     MOVWF	CT1
     GOTO		LOOP
 ;--------------------------------------RUTINAS-----------------------------------------
 AJUSTE_CT
     MOVWF	ROTF			;Se recupera el valor en W
-    BCF		STATUS, 0		;Se asegura que carry este en 0.
-    RRF		ROTF, W		;Se procede a rotar a la derecha para emular la division
+    BCF		STATUS, C		;Se asegura que carry este en 0.
+    RRF		ROTF,  F		;Se procede a rotar a la derecha para emular la division
 					;ahora el valor esta entre 0 - 127.
-    ADDLW	.97			;Se adiciona 97 para que el tiempo en el TRM0, este
-					;entre 0.512ms < t < 2.544ms. 
-    RETURN
+    MOVLW	.31			;Se adiciona 97 para que el tiempo en el TRM0, este
+    ADDWF	ROTF, F			;entre 0.512ms < t < 2.544ms. 
+    RETURN    
 DELAY_4US				;DELAY DE  4us (supuestamente)
     BCF		STATUS,5
     BCF		STATUS,6
